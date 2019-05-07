85ef4226791c6b44763d14395c29a9da951d9ef9
ripsildeveloper <ripsildeveloper3@gmail.com>
2019-05-07 14:27:01 +0530

    appsetting serverpath changes

--- src/config/appSetting.js
+++ src/config/appSetting.js
@@ -1,9 +1,15 @@
 module.exports = {
-  productServerPath: 'http://localhost/Lencott/products/',
+  /* productServerPath: 'http://localhost/Lencott/products/',
   bannerServerPath: 'http://localhost/Lencott/banners/',
   adsServerPath: 'http://localhost/Lencott/ads/',
   categoryServerPath: 'http://localhost/Lencott/category/',
   logoServerPath: 'http://localhost/Lencott/logo/',
   headerUploadPath: 'images/header',
-  headerServerPath: 'http://localhost/Lencott/header/'
+  headerServerPath: 'http://localhost/Lencott/header/' */
+  productServerPath: 'https://benjolb2b.com/admin/images/product/',
+  bannerServerPath: 'https://benjolb2b.com/admin/images/banner/',
+  adsServerPath: 'https://benjolb2b.com/admin/images/ads/',
+  logoServerPath: 'https://benjolb2b.com/admin/images/logo/',
+  headerServerPath: '/home/ubuntu/my-folder/app/seller-app/images/header' */
 };
+
