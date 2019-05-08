2d9d4f81281edab68bd2778ac3f99d73b8c3231a
ripsildeveloper <ripsildeveloper3@gmail.com>
2019-05-08 09:10:03 +0530

    changes in db

--- src/config/appSetting.js
+++ src/config/appSetting.js
@@ -1,11 +1,11 @@
 module.exports = {
-  /* productServerPath: 'http://localhost/Lencott/products/',
-  bannerServerPath: 'http://localhost/Lencott/banners/',
-  adsServerPath: 'http://localhost/Lencott/ads/',
-  categoryServerPath: 'http://localhost/Lencott/category/',
-  logoServerPath: 'http://localhost/Lencott/logo/',
+  /* productServerPath: 'http://localhost/Benjolb2b/products/',
+  bannerServerPath: 'http://localhost/Benjolb2b/banners/',
+  adsServerPath: 'http://localhost/Benjolb2b/ads/',
+  categoryServerPath: 'http://localhost/Benjolb2b/category/',
+  logoServerPath: 'http://localhost/Benjolb2b/logo/',
   headerUploadPath: 'images/header',
-  headerServerPath: 'http://localhost/Lencott/header/' */
+  headerServerPath: 'http://localhost/Benjolb2b/header/' */
   productServerPath: 'https://benjolb2b.com/admin/images/product/',
   bannerServerPath: 'https://benjolb2b.com/admin/images/banner/',
   adsServerPath: 'https://benjolb2b.com/admin/images/ads/',
