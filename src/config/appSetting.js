d536b48420d77de2783f097887f2323354d7dfcd
ripsildeveloper <ripsildeveloper3@gmail.com>
2019-05-08 09:56:58 +0530

    changes in ui

--- src/config/appSetting.js
+++ src/config/appSetting.js
@@ -6,10 +6,10 @@ module.exports = {
   logoServerPath: 'http://localhost/Benjolb2b/logo/',
   headerUploadPath: 'images/header',
   headerServerPath: 'http://localhost/Benjolb2b/header/' */
-  productServerPath: 'https://benjolb2b.com/admin/images/product/',
-  bannerServerPath: 'https://benjolb2b.com/admin/images/banner/',
-  adsServerPath: 'https://benjolb2b.com/admin/images/ads/',
-  logoServerPath: 'https://benjolb2b.com/admin/images/logo/',
-  headerServerPath: '/home/ubuntu/my-folder/app/seller-app/images/header'
+  productServerPath: 'http://benjolb2b.com/admin/images/product/',
+  bannerServerPath: 'http://benjolb2b.com/admin/images/banner/',
+  adsServerPath: 'http://benjolb2b.com/admin/images/ads/',
+  logoServerPath: 'http://benjolb2b.com/admin/images/logo/',
+  headerServerPath: 'http://benjolb2b.com/admin/images/header/'
 };
 
